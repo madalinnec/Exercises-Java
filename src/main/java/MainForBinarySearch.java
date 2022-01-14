08acecd40923f7a36d93bf264ae27139673a7256
MadaNec <madalinneculaes@gmail.com>
2022-01-14 14:13:44 +0200

    Smallest difference between two array elements added

--- src/main/java/MainForBinarySearch.java
+++ src/main/java/MainForBinarySearch.java
@@ -1,2 +1,16 @@
 public class MainForBinarySearch {
+    public static void main(String[] args) {
+        int[] array ={12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+        int elementToFindIndexFor = 29;
+    }
+//    public static int findUsingBinarySearch(int[] array, int elementToFindIndexFor){
+//        int smallestIndex = 0;
+//        int highestIndex = array.length-1;
+//        int middleIndex = array.length/2;
+//        while(smallestIndex <= highestIndex){
+//            if (elementToFindIndexFor == array) {
+//
+//            }
+//        }
+//    }
 }
---
+++ src/main/java/MainForSmallestDifferenceInArray.java
@@ -0,0 +1,22 @@
+public class MainForSmallestDifferenceInArray {
+    public static void main(String[] args) {
+        int[] array = {12,34,25,67,86,42,11,24,5,2,124,6,8,86,89};
+        findSmallestDifferenceElements(array);
+    }
+    public static void findSmallestDifferenceElements(int[] array){
+        int smallestDifference = Integer.MAX_VALUE;
+        int firstElement = 0;
+        int secondElement = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if(i != j && array[i]-array[j]<smallestDifference){
+                    smallestDifference = array[i] - array[j];
+                    firstElement = array[i];
+                    secondElement = array[j];
+                }
+            }
+        }
+        System.out.println("The elements with the smallest difference, " + smallestDifference +
+                            " are " + firstElement + " and " + secondElement);
+    }
+}

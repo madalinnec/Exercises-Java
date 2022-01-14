050f91382735350d30ab94feed625d566db65e61
MadaNec <madalinneculaes@gmail.com>
2022-01-14 15:04:34 +0200

    Added method for the fibonacci number

---
+++ src/main/java/MainForFibonacci.java
@@ -0,0 +1,19 @@
+public class MainForFibonacci {
+    public static void main(String[] args) {
+        int highestValueToBeReached = 2000;
+        createFibonacci(highestValueToBeReached);
+    }
+
+    public static void createFibonacci(int x){
+        int firstElement = 0;
+        System.out.println(firstElement);
+        int secondElement = 1;
+        int nextElement = 1;
+        while (nextElement <= x ){
+            System.out.println(nextElement);
+            nextElement = firstElement + secondElement;
+            firstElement = secondElement;
+            secondElement = nextElement;
+        }
+    }
+}

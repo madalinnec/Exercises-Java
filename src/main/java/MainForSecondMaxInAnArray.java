6478221688f9bf4f3ee7315a63a6be5c48085878
MadaNec <madalinneculaes@gmail.com>
2022-01-14 14:49:00 +0200

    Added method for finding the second max in an array

---
+++ src/main/java/MainForSecondMaxInAnArray.java
@@ -0,0 +1,24 @@
+public class MainForSecondMaxInAnArray {
+    public static void main(String[] args) {
+        int[] array = {5, 3, 2, 65, 647, 23, 12, 7, 974534, 23, 56, 753, 112, 22, 45, 0};
+        findSecondMaxInArray(array);
+    }
+
+    public static void findSecondMaxInArray(int[] array) {
+        int maxValueInArray = array[0];
+        int secondMaxValueInArray = array[0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j] && maxValueInArray < array[j]) {
+                    maxValueInArray = array[j];
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (secondMaxValueInArray < array[i] && maxValueInArray > array[i]) {
+                secondMaxValueInArray = array[i];
+            }
+        }
+        System.out.println("The second max value in this array is: " + secondMaxValueInArray);
+    }
+}

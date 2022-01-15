5da2acadd91e8ce107ee8185240e4d56471e9b01
Madalin Neculaes <madalinneculaes@Madalins-MacBook-Pro.local>
2022-01-15 23:04:07 +0200

    Created method for checking if a String is a palindrome

--- src/main/java/MainForStringPalindrome.java
+++ src/main/java/MainForStringPalindrome.java
@@ -1,2 +1,22 @@
 public class MainForStringPalindrome {
+    public static void main(String[] args) {
+        String string = "aba";
+        checkIfPalindrome(string);
+    }
+
+    public static void checkIfPalindrome(String string) {
+        String reversedString ="";
+        boolean palindrome = false;
+        char aux;
+        for (int i = string.length() - 1; i >= 0; i--) {
+            reversedString += string.charAt(i);
+        }
+        if(string.equals(reversedString)){
+            palindrome =  true;
+            System.out.println(palindrome);
+        }else{
+            System.out.println(palindrome);
+        }
+    }
 }
+

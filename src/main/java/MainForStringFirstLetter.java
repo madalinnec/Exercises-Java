f0fafa19791a1701cad6437584b124141fb83bd3
Madalin Neculaes <madalinneculaes@Madalins-MacBook-Pro.local>
2022-01-17 11:28:16 +0200

    method for retrieving first letter of words in a string - done

--- src/main/java/MainForStringFirstLetter.java
+++ src/main/java/MainForStringFirstLetter.java
@@ -1,18 +1,22 @@
 public class MainForStringFirstLetter {
     public static void main(String[] args) {
-        String words = "Let's see if this actually works and an a";
+        String words = "Let's see if this actually works and a c";
         getFirstLetters(words);
     }
 
     public static void getFirstLetters(String words) {
+        words = words.trim();
         String auxString = "";
         char auxChar = ' ';
+        char firstChar = words.charAt(0);
+        if(Character.isLetter(firstChar)){
+            System.out.println(firstChar);
+        }
         for (int i = 0; i < words.length(); i++) {
             auxChar = words.charAt(i);
             if (auxChar == ' ') {
                 System.out.println(words.charAt(i+1));
             }
-            System.out.println(auxString);
         }
     }
 }
